d44b1e3d64d0332e06890eda5e1c0fc07e9a6471
Hamid Rezaeirad <hamid.rezaeirad@gmail.com>
2024-06-16 15:39:57 +0200

    fix any type and clean up api.server (#2)

--- Frontend/src/server/api.srver.tsx
+++ Frontend/src/server/api.srver.tsx
@@ -1,4 +1,4 @@
-import axios, { Method, AxiosResponse } from "axios";
+import axios from "axios";
 
 /**
  *
@@ -7,19 +7,5 @@ import axios, { Method, AxiosResponse } from "axios";
 const api = axios.create({
   baseURL: "http://localhost:3000/api", // later need to read from process.env REACT_APP_HOST_BACKEND,
 });
-
-// https://dev.to/djamaile/how-i-structure-my-react-ts-applications-160g
-
-// const request = <T,>(
-//   method: Method,
-//   url: string,
-//   params: any
-// ): Promise<AxiosResponse<T>> => {
-//   return api.request<T>({
-//     method,
-//     url,
-//     params,
-//   });
-// };
 
 export default api;
